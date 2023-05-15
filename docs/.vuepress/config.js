7a1b0c73d6336011e8382b84f393f4de7533aca4
chenqifeng66 <839860616@qq.com>
2023-05-15 15:42:29 +0800

    change theme

--- docs/.vuepress/config.js
+++ docs/.vuepress/config.js
@@ -1,33 +1,20 @@
 import { defineUserConfig, defaultTheme } from "vuepress";
+import { hopeTheme } from "vuepress-theme-hope";
+import { searchProPlugin } from "vuepress-plugin-search-pro";
 
 export default defineUserConfig({
   base: "/MyBlog/",
   lang: "zh-CN",
   title: "MyBlog",
   description: "欢迎来到我的个人博客",
-  theme: defaultTheme({
+  name: "reco",
+  theme: hopeTheme({
     // logo
     logo: "/images/fox.svg",
     // github地址
-    repo: "https://github.com/chenqifeng66",
+    repo: "https://github.com/chenqifeng66/MyBlog",
     // 主页
     home: "/",
-    // 默认主题配置
-    // locales: {
-    //   "en-US": {
-    //     navbar: [
-    //       {
-    //         text: "HOME",
-    //         link: "/",
-    //       },
-    //     ],
-    //     colorModeSwitch: true,
-    //     colorMode: "dark",
-    //     logo: "http://rongapi.cn/images/logo.png",
-    //     repo: "http://gitee.org/vuejs/vuepress",
-    //     repoLabel: "Source",
-    //   },
-    // },
     navbar: [
       {
         text: "介绍",
@@ -51,6 +38,28 @@ export default defineUserConfig({
         ],
       },
     ],
+    plugins: {
+      copyCode: {},
+      git: true,
+    },
     sidebar: "auto",
   }),
+  plugins: [
+    // 搜索功能
+    searchProPlugin({
+      // 索引全部内容
+      indexContent: true,
+      // 为分类和标签添加索引
+      customFields: [
+        {
+          getter: (page) => page.frontmatter.category,
+          formatter: "分类：$content",
+        },
+        {
+          getter: (page) => page.frontmatter.tag,
+          formatter: "标签：$content",
+        },
+      ],
+    }),
+  ],
 });
